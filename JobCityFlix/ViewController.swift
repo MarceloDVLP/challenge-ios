b63206f194be2f67105920b9b29cb09d487b9078
your_username <your_email@gmail.com>
2022-08-27 18:13:14 -0300

    Initial Commit

---
+++ JobCityFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobCityFlix
+//
+//  Created by Marcelo Carvalho on 27/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
